895fc3c0c9a0434a6f73b3ab8e42d5465198c7e7
Andrew Welch <andrew@nystudio107.com>
2018-06-29 20:50:24 -0400

    Version 0.1.3
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 import path from 'path'
 import critical from 'critical'
 
-export default function CriticalCssWebpackPlugin (options = {}) {
-  this.options = Object.assign({
-    src: 'index.html',
-    dest: 'index.html',
-    inline: true,
-    minify: true,
-    extract: true,
-    width: 375,
-    height: 565,
-    penthouse: {
-      blockJSRequests: false
-    }
-  }, options)
+export default function CriticalCssWebpackPlugin(options = {}) {
+    this.options = Object.assign({
+        src: 'index.html',
+        dest: 'index.html',
+        inline: true,
+        minify: true,
+        extract: true,
+        width: 375,
+        height: 565,
+        penthouse: {
+            blockJSRequests: false
+            phantomJsOptions: {
+                'ssl-protocol': 'any',
+            }
+        }
+    }, options)
 }
 
-CriticalCssWebpackPlugin.prototype.apply = function (compiler) {
-  compiler.plugin('after-emit', (compilation, callback) => {
-    const base = compilation.outputOptions.path
+CriticalCssWebpackPlugin.prototype.apply = function(compiler) {
+    compiler.plugin('after-emit', (compilation, callback) => {
+        const base = compilation.outputOptions.path
 
-    const css = Object.keys(compilation.assets)
-      .filter(filename => /\.css$/.test(filename))
-      .map(filename => path.join(base, filename))
+        const css = Object.keys(compilation.assets)
+            .filter(filename => /\.css$/.test(filename))
+            .map(filename => path.join(base, filename))
 
-    critical.generate(Object.assign({ base, css }, this.options), callback)
-  })
+        critical.generate(Object.assign({base, css}, this.options), callback)
+    })
 }
